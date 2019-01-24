cf0c41f4bff5509e0629f188d6942cad98a229e0
Dawid <dawidmt@gmail.com>
2019-01-24 16:11:50 +0100

    -fix bitfinex ExchangeSpecs color

--- src/main/java/com/dawidmotyka/exchangeutils/exchangespecs/BitfinexExchangeSpecs.java
+++ src/main/java/com/dawidmotyka/exchangeutils/exchangespecs/BitfinexExchangeSpecs.java
@@ -11,7 +11,7 @@ public class BitfinexExchangeSpecs extends ExchangeSpecs {
 
     private static final String EXCHANGE_NAME = "Bitfinex";
     private static final String MARKET_URL = "https://www.bitfinex.com/t/";
-    private static final String COLOR_HEX = " 32CD32";
+    private static final String COLOR_HEX = "32CD32";
 
     public BitfinexExchangeSpecs() {
         super(EXCHANGE_NAME,MARKET_URL, BitfinexExchange.class,0.0005, COLOR_HEX,6000);
