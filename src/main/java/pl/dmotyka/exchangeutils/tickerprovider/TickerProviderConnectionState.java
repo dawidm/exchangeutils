777123e31bef0937a16067e9c6841218d6d9d4dd
Dawid Motyka <dawidmt@gmail.com>
2020-10-30 02:48:55 +0100

    Ticker connection state comments

--- src/main/java/pl/dmotyka/exchangeutils/tickerprovider/TickerProviderConnectionState.java
+++ src/main/java/pl/dmotyka/exchangeutils/tickerprovider/TickerProviderConnectionState.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose2
  *
- * Copyright © 2019 Dawid Motyka
+ * Copyright © 2019-2020 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -13,6 +13,17 @@
 
 package pl.dmotyka.exchangeutils.tickerprovider;
 
+// State of TickerProvider connection
+// After connecting, TickerProvider should keep the connection active till requesting disconnect
+// It should automatically reconnect in case of any connection problems
 public enum TickerProviderConnectionState {
-    CONNECTED, DISCONNECTED, RECONNECTING
+    // Connection established
+    // Should be sent after successful connection/reconnection
+    CONNECTED,
+    // Connection closed
+    // Should only be sent after requested disconnection
+    DISCONNECTED,
+    // Reconnecting
+    // Sent in case of broken connection, when trying to reconnect
+    RECONNECTING
 }
