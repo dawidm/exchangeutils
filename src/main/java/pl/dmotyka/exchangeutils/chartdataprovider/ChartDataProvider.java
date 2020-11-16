e9979ca86e2b6c2b0b38354f1411904763d766fd
Dawid Motyka <dawidmt@gmail.com>
2020-11-16 21:33:00 +0100

    fix aborting chart data provider

--- src/main/java/pl/dmotyka/exchangeutils/chartdataprovider/ChartDataProvider.java
+++ src/main/java/pl/dmotyka/exchangeutils/chartdataprovider/ChartDataProvider.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose2
  *
- * Copyright © 2019 Dawid Motyka
+ * Copyright © 2019-2020 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -80,11 +80,11 @@ public class ChartDataProvider {
         isRefreshingChartData.set(true);
         for (String currentPair : pairs) {
             for(PeriodNumCandles currentPeriodNumCandles : periodsNumCandles) {
-                if(abortAtomicBoolean.get()==true)
-                    return;
+                if(abortAtomicBoolean.get())
+                    break;
                 logger.fine("getting data for " + currentPair);
                 RepeatTillSuccess.planTask(() -> {
-                            if (abortAtomicBoolean.get() == true)
+                            if (abortAtomicBoolean.get())
                                 return;
                             getChartData(currentPair, currentPeriodNumCandles.getNumCandles(), currentPeriodNumCandles.getPeriodSeconds());
                         },
