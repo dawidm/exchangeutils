3b050e62be5aa243074d8964c762756c5407b10b
Dawid Motyka <dawidmt@gmail.com>
2020-11-26 17:14:07 +0100

    cleanup

--- src/main/java/pl/dmotyka/exchangeutils/chartinfo/ChartCandle.java
+++ src/main/java/pl/dmotyka/exchangeutils/chartinfo/ChartCandle.java
@@ -1,7 +1,7 @@
 /*
  * Cryptonose2
  *
- * Copyright © 2019 Dawid Motyka
+ * Copyright © 2019-2020 Dawid Motyka
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
@@ -15,11 +15,11 @@ package pl.dmotyka.exchangeutils.chartinfo;
 
 public class ChartCandle {
 
-    private double high;
-    private double low;
-    private double open;
-    private double close;
-    private long timestampSeconds;
+    private final double high;
+    private final double low;
+    private final double open;
+    private final double close;
+    private final long timestampSeconds;
 
     public ChartCandle(double high, double low, double open, double close, long timestampSeconds) {
         this.timestampSeconds=timestampSeconds;
