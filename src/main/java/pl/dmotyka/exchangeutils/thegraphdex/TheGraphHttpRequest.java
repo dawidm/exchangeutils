56d725c9fc42bdd88c21eb912190131e90ff3640
Dawid Motyka <dawidmt@gmail.com>
2021-11-23 17:36:13 +0100

    started implementing the graph dex exchanges * implemented an api to send requests to the graph * implemented a simple request - for coins pairs (pools) * added a test for pairs request

---
+++ src/main/java/pl/dmotyka/exchangeutils/thegraphdex/TheGraphHttpRequest.java
@@ -0,0 +1,61 @@
+/*
+ * Cryptonose
+ *
+ * Copyright © 2019-2021 Dawid Motyka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package pl.dmotyka.exchangeutils.thegraphdex;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.dmotyka.exchangeutils.exceptions.ExchangeCommunicationException;
+
+public class TheGraphHttpRequest {
+
+    private final TheGraphExchangeSpecs theGraphExchangeSpecs;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static final String ERRORS_FIELD = "errors";
+    public static final String DATA_FIELD = "data";
+
+    public TheGraphHttpRequest(TheGraphExchangeSpecs theGraphExchangeSpecs) {
+        this.theGraphExchangeSpecs = theGraphExchangeSpecs;
+    }
+
+    public JsonNode send(TheGraphQuery theGraphQuery) throws ExchangeCommunicationException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                         .uri(URI.create(theGraphExchangeSpecs.getTheGraphApiURL()))
+                                         .header("Content-Type", "application/json")
+                                         .header("Accept", "application/json")
+                                         .POST(HttpRequest.BodyPublishers.ofString(theGraphQuery.getJSONQueryString()))
+                                         .build();
+        try {
+            String responseBody = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            JsonNode returnNode = objectMapper.readValue(responseBody, JsonNode.class);
+            if (returnNode.has(ERRORS_FIELD)) {
+                throw new ExchangeCommunicationException(returnNode.get(ERRORS_FIELD).asText());
+            }
+            if (returnNode.has(DATA_FIELD)) {
+                return returnNode.get(DATA_FIELD);
+            } else {
+                throw new ExchangeCommunicationException("no data field returned");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new ExchangeCommunicationException("when sending request to The Graph", e);
+        }
+    }
+}
---
+++ src/main/java/thegraphuniswapv3/UniswapExchangeSpecs.java
@@ -0,0 +1,86 @@
+/*
+ * Cryptonose
+ *
+ * Copyright © 2019-2021 Dawid Motyka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package thegraphuniswapv3;
+
+import org.knowm.xchange.Exchange;
+import pl.dmotyka.exchangeutils.chartinfo.ExchangeChartInfo;
+import pl.dmotyka.exchangeutils.pairdataprovider.PairDataProvider;
+import pl.dmotyka.exchangeutils.pairsymbolconverter.PairSymbolConverter;
+import pl.dmotyka.exchangeutils.thegraphdex.TheGraphExchangeSpecs;
+import pl.dmotyka.exchangeutils.tickerprovider.TickerProvider;
+import pl.dmotyka.exchangeutils.tickerprovider.TickerReceiver;
+
+public class UniswapExchangeSpecs extends TheGraphExchangeSpecs {
+
+    private static final String THE_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3";
+
+    public UniswapExchangeSpecs() {
+        super("Uniswap3");
+    }
+
+    @Override
+    public ExchangeChartInfo getChartInfo() {
+        return null;
+    }
+
+    @Override
+    public PairDataProvider getPairDataProvider() {
+        return null;
+    }
+
+    @Override
+    public PairSymbolConverter getPairSymbolConverter() {
+        return null;
+    }
+
+    @Override
+    public String getMarketUrl() {
+        return null;
+    }
+
+    @Override
+    public Exchange getXchangeExchange() {
+        return null;
+    }
+
+    @Override
+    public String getColorHash() {
+        return null;
+    }
+
+    @Override
+    public int getDelayBetweenChartDataRequestsMs() {
+        return 0;
+    }
+
+    @Override
+    public TickerProvider getTickerProvider(TickerReceiver tickerReceiver, String[] pairs) {
+        return null;
+    }
+
+    @Override
+    public String getApiHostname() {
+        return null;
+    }
+
+    @Override
+    public int getApiPort() {
+        return 0;
+    }
+
+    @Override
+    public String getTheGraphApiURL() {
+        return THE_GRAPH_URL;
+    }
+}
