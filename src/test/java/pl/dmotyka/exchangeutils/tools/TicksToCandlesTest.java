b2c21c4aff218924bb0c17157cd2478119f19886
Dawid Motyka <dawidmt@gmail.com>
2021-11-25 19:07:53 +0100

    modify candles generation test to use tickers generated offline

--- src/test/java/pl/dmotyka/exchangeutils/tools/TicksToCandlesTest.java
+++ src/test/java/pl/dmotyka/exchangeutils/tools/TicksToCandlesTest.java
@@ -14,42 +14,31 @@
 package pl.dmotyka.exchangeutils.tools;
 
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
 import pl.dmotyka.exchangeutils.chartinfo.ChartCandle;
 import pl.dmotyka.exchangeutils.exceptions.ExchangeCommunicationException;
-import pl.dmotyka.exchangeutils.pairsymbolconverter.PairSymbolConverter;
-import pl.dmotyka.exchangeutils.thegraphdex.TheGraphHttpRequest;
-import pl.dmotyka.exchangeutils.thegraphuniswapv3.Uniswap3ExchangeSpecs;
-import pl.dmotyka.exchangeutils.thegraphuniswapv3.Uniswap3SwapsQuery;
-import pl.dmotyka.exchangeutils.thegraphuniswapv3.Uniswap3SwapsToTickers;
 import pl.dmotyka.exchangeutils.tickerprovider.Ticker;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TicksToCandlesTest {
 
     @Test
     void generateCandles() throws ExchangeCommunicationException {
         long periodSeconds = 3600;
-        TheGraphHttpRequest req = new TheGraphHttpRequest(new Uniswap3ExchangeSpecs());
-        PairSymbolConverter pairSymbolConverter = new Uniswap3ExchangeSpecs().getPairSymbolConverter();
-        // USDC/WETH and WETH/USDT pools
-        String[] pools = new String[] {"0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8", "0x11b815efb8f581194ae79006d24e0d814b7697f6"};
         int currentTimestamp = (int) Instant.now().getEpochSecond();
         int dayAgoTimestamp = currentTimestamp - 24*60*60;
-        Uniswap3SwapsQuery swapsQuery = new Uniswap3SwapsQuery(pools, dayAgoTimestamp, currentTimestamp);
-        List<JsonNode> resultNodes = req.send(swapsQuery);
+        double tmpPrice = 100;
         List<Ticker> tickersList = new LinkedList<>();
-        for(JsonNode resultNode : resultNodes) {
-            Ticker[] tickers = Uniswap3SwapsToTickers.generateTickers(resultNode);
-            tickersList.addAll(Arrays.asList(tickers));
+        for(int tmpTimestamp = dayAgoTimestamp; tmpTimestamp < currentTimestamp; tmpTimestamp += 1 + (int)(Math.random()*10)) {
+            tmpPrice = tmpPrice + Math.random()*2-1;
+            tickersList.add(new Ticker("TEST", tmpPrice, tmpTimestamp));
         }
-        ChartCandle[] candles = TicksToCandles.generateCandles(tickersList.stream().filter(t -> t.getPair().startsWith("WETH_")).toArray(Ticker[]::new), periodSeconds);
+        ChartCandle[] candles = TicksToCandles.generateCandles(tickersList.toArray(Ticker[]::new), periodSeconds);
         assertNotNull(candles);
         assertTrue(candles.length > 0);
         for (ChartCandle candle : candles) {
